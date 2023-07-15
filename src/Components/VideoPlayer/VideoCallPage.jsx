31d8094cf6db41b8317ad1347653785073bb3ceb
SNEHA CHAUHAN <sneha2003chauhan@gmail.com>
2023-07-15 10:02:56 +0530

    Video uploaded

--- src/Components/VideoPlayer/VideoCallPage.jsx
+++ src/Components/VideoPlayer/VideoCallPage.jsx
@@ -18,13 +18,13 @@ function VideoCallPage() {
       <Box
         sx={{
           position: 'relative',
-          paddingTop: '75%',
+          paddingTop: '20px',
           width: '100%',
           maxWidth: 300,
           margin: '0 auto',
         }}
       >
-        <ReactPlayer url="https://www.youtube.com/shorts/0u0gyT23JsU" width="100%" height="100%" controls={true} />
+        <ReactPlayer url="https://res.cloudinary.com/dx0dgujbj/video/upload/v1689395456/%E0%A4%AA%E0%A4%B9%E0%A4%B2%E0%A5%80-%E0%A4%AC%E0%A4%BE%E0%A4%B0-%E0%A4%AE%E0%A5%87-%E0%A4%95%E0%A5%81%E0%A4%9B-%E0%A4%A8%E0%A4%B9%E0%A5%80-%E0%A4%B9%E0%A5%8B%E0%A4%A4%E0%A4%BE-_-Sonu-Sharma-Success-Motivation-__motivational-_short-_shorts-_shortsvideo720P_HD_bkftpt.mp4" width="100%" height="100%" controls={true} />
       </Box>
 
       <Box
